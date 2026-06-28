d76ae42f6f5aea216c84fd0964d5b29865237c37
MJ12358 <wtpltlf.dev@gmail.com>
2026-06-27 20:11:03 -0500

    remove constructors from CustomReorderableListView, as these were fixed in a Flutter update

--- lib/src/reorderable_list.dart
+++ lib/src/reorderable_list.dart
@@ -1,55 +1,13 @@
 part of '../flutter_widgetz.dart';
 
 /// {@template flutter_widgetz.CustomReorderableListView}
-/// Wraps [ReorderableListView] to allow a separator and fix
-/// indexing issue in the original Flutter widget.
-///
-/// https://github.com/flutter/flutter/issues/24786
+/// Wraps [ReorderableListView] to allow a separator.
 ///
 /// Inspired by: https://github.com/flutter/flutter/issues/76706
 ///
 ///![CustomReorderableListView](https://raw.githubusercontent.com/MJ12358/flutter-widgetz/main/screenshots/list_view.png)
 /// {@endtemplate}
 class CustomReorderableListView extends ReorderableListView {
-  /// {@macro flutter_widgetz.CustomReorderableListView}
-  CustomReorderableListView({
-    super.key,
-    required super.children,
-    required ReorderCallback onReorder,
-    super.clipBehavior,
-    super.footer,
-    super.header,
-    super.padding,
-    super.physics,
-    super.proxyDecorator,
-    super.shrinkWrap,
-  }) : super(
-         onReorder: (int oldIndex, int newIndex) {
-           _defaultReorderCallback(oldIndex, newIndex, onReorder);
-         },
-       );
-
-  /// Builder uses [ReorderableListView.builder].
-  ///
-  /// {@macro flutter_widgetz.CustomReorderableListView}
-  CustomReorderableListView.builder({
-    super.key,
-    required super.itemCount,
-    required super.itemBuilder,
-    required ReorderCallback onReorder,
-    super.clipBehavior,
-    super.footer,
-    super.header,
-    super.padding,
-    super.physics,
-    super.proxyDecorator,
-    super.shrinkWrap,
-  }) : super.builder(
-         onReorder: (int oldIndex, int newIndex) {
-           _defaultReorderCallback(oldIndex, newIndex, onReorder);
-         },
-       );
-
   /// Separated uses [ReorderableListView.builder] with separators.
   ///
   /// {@macro flutter_widgetz.CustomReorderableListView}
@@ -81,7 +39,7 @@ class CustomReorderableListView extends ReorderableListView {
            }
            return itemBuilder.call(context, index ~/ 2);
          },
-         onReorder: (int oldIndex, int newIndex) {
+         onReorderItem: (int oldIndex, int newIndex) {
            int oi = oldIndex;
            int ni = newIndex;
            if (oi < ni) {
@@ -105,18 +63,5 @@ class CustomReorderableListView extends ReorderableListView {
   // for the separated constructor.
   static int _computeActualChildCount(int itemCount) {
     return math.max(0, itemCount * 2 - 1);
-  }
-
-  // Helper method to adjust indices for reordering.
-  static void _defaultReorderCallback(
-    int oldIndex,
-    int newIndex,
-    ReorderCallback onReorder,
-  ) {
-    int adjustedIndex = newIndex;
-    if (oldIndex < newIndex) {
-      adjustedIndex -= 1;
-    }
-    onReorder(oldIndex, adjustedIndex);
   }
 }
